c0a6eb891710700ba64a46773772996ebdc86133
Taufiiqul Hakim <51141717+taufik230301@users.noreply.github.com>
2022-06-12 12:41:50 +0700

    add view login

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -1,11 +1,11 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class Login extends CI_Controller {
 
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		$this->load->view('login');
 	}
 	
 }
