11bb4f8138222672778bd4ab84fff1ad19a8bdc2
Taufiiqul Hakim <51141717+taufik230301@users.noreply.github.com>
2022-06-12 12:24:35 +0700

    add view public

--- application/controllers/Public_Page.php
+++ application/controllers/Public_Page.php
@@ -1,11 +1,11 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class Public_Page extends CI_Controller {
 
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		$this->load->view('public');
 	}
 	
 }
