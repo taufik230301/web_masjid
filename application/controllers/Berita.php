8f47eecdbad936fe785fd7c5154a0c0e752644ad
Taufiiqul Hakim <51141717+taufik230301@users.noreply.github.com>
2022-06-13 10:38:24 +0700

    add view berita

---
+++ application/controllers/Berita.php
@@ -0,0 +1,13 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Berita extends CI_Controller {
+
+	public function view_admin()
+	{
+		$this->load->view('admin/berita');
+    }
+    
+   
+	
+}
