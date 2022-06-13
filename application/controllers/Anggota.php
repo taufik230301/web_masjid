4a3d819b850ff026f7115d04f57890874545d81e
Taufiiqul Hakim <51141717+taufik230301@users.noreply.github.com>
2022-06-13 10:54:23 +0700

    add view anggota

---
+++ application/controllers/Anggota.php
@@ -0,0 +1,12 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Anggota extends CI_Controller {
+
+	public function view_admin()
+	{
+		$this->load->view('admin/anggota');
+    }
+    
+	
+}
