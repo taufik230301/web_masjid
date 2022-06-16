766d409d41a04602159013773104efe9655d0154
Taufiiqul Hakim <51141717+taufik230301@users.noreply.github.com>
2022-06-16 13:42:30 +0700

    add inventory data in controller

--- application/controllers/Inventory.php
+++ application/controllers/Inventory.php
@@ -3,13 +3,24 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Inventory extends CI_Controller {
 
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('m_inventory');
+	}
+
 	public function view_admin()
 	{
+		$data['inventory'] = $this->m_inventory->read_all_inventory()->result_array();
+
 		$this->load->view('admin/inventory');
+
     }
 
     public function view_anggota()
 	{
+		$data['inventory'] = $this->m_inventory->read_all_inventory()->result_array();
+
 		$this->load->view('anggota/inventory');
     }
 	
