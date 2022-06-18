3f9ffe43cfee454b4b2eefcc53b88a85ad606eb2
Taufiiqul Hakim <51141717+taufik230301@users.noreply.github.com>
2022-06-18 17:10:29 +0700

    add disable when upload data anggota

--- application/views/anggota/lengkapi_data.php
+++ application/views/anggota/lengkapi_data.php
@@ -94,21 +94,36 @@
                                 <div class="form-group">
                                     <label for="nama_lengkap">Nama Lengkap</label>
                                     <input type="text" class="form-control" id="nama_lengkap" name="nama_lengkap"
-                                        value="<?=$nama_lengkap?>" required>
+                                        value="<?=$nama_lengkap?>" <?php  if( $id_status_verifikasi == '2' OR $id_status_verifikasi == '4'){
+                                        echo 'disabled';
+                                    }else{
+                                        echo 'required';
+                                    }  ?>>
                                 </div>
                                 <div class="form-group">
                                     <label for="no_kk">Nomor Kartu Keluarga</label>
-                                    <input type="text" class="form-control" id="no_kk" name="no_kk" value="<?=$no_kk?>"
-                                        required>
+                                    <input type="text" class="form-control" id="no_kk" name="no_kk" value="<?=$no_kk?>" <?php  if( $id_status_verifikasi == '2' OR $id_status_verifikasi == '4'){
+                                        echo 'disabled';
+                                    }else{
+                                        echo 'required';
+                                    }  ?>>
                                 </div>
                                 <div class="form-group">
                                     <label for="no_ktp">Nomor KTP</label>
                                     <input type="text" class="form-control" id="no_ktp" name="no_ktp"
-                                        value="<?=$no_ktp?>" required>
+                                        value="<?=$no_ktp?>" <?php  if( $id_status_verifikasi == '2' OR $id_status_verifikasi == '4'){
+                                        echo 'disabled';
+                                    }else{
+                                        echo 'required';
+                                    }  ?>>
                                 </div>
                                 <div class="form-group">
                                     <label for="jenis_kelamin">Jenis Kelamin</label>
-                                    <select class="form-control" id="jenis_kelamin" name="jenis_kelamin" required>
+                                    <select class="form-control" id="jenis_kelamin" name="jenis_kelamin" <?php  if( $id_status_verifikasi == '2' OR $id_status_verifikasi == '4'){
+                                        echo 'disabled';
+                                    }else{
+                                        echo 'required';
+                                    }  ?>>
                                         <option value="L" <?php  if($jenis_kelamin == 'L'){
                                                                                                 echo 'selected';
                                                                                             }else{
@@ -123,33 +138,56 @@
                                 </div>
                                 <div class="form-group">
                                     <label for="agama">Agama</label>
-                                    <input type="text" class="form-control" id="agama" name="agama" value="<?=$agama?>"
-                                        required>
+                                    <input type="text" class="form-control" id="agama" name="agama" value="<?=$agama?>" <?php  if( $id_status_verifikasi == '2' OR $id_status_verifikasi == '4'){
+                                        echo 'disabled';
+                                    }else{
+                                        echo 'required';
+                                    }  ?>>
                                 </div>
                                 <div class="form-group">
                                     <label for="no_hp">Nomor HP</label>
                                     <input type="text" class="form-control" id="no_hp" name="no_hp"
-                                        value="<?=$nama_lengkap?>" required>
+                                        value="<?=$nama_lengkap?>" <?php  if( $id_status_verifikasi == '2' OR $id_status_verifikasi == '4'){
+                                        echo 'disabled';
+                                    }else{
+                                        echo 'required';
+                                    }  ?>>
                                 </div>
                                 <div class="form-group">
                                     <label for="alamat">Alamat</label>
                                     <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"
-                                        name="alamat" required><?=$alamat?></textarea>
+                                        name="alamat" <?php  if( $id_status_verifikasi == '2' OR $id_status_verifikasi == '4'){
+                                        echo 'disabled';
+                                    }else{
+                                        echo 'required';
+                                    }  ?>><?=$alamat?></textarea>
                                 </div>
                                 <div class="form-group">
                                     <label for="tempat_lahir">Tempat Lahir</label>
                                     <input type="text" class="form-control" id="tempat_lahir" name="tempat_lahir"
-                                        value="<?=$tempat_lahir?>" required>
+                                        value="<?=$tempat_lahir?>" <?php  if( $id_status_verifikasi == '2' OR $id_status_verifikasi == '4'){
+                                        echo 'disabled';
+                                    }else{
+                                        echo 'required';
+                                    }  ?>>
                                 </div>
                                 <div class="form-group">
                                     <label for="tanggal_lahir">Tanggal Lahir</label>
                                     <input type="date" class="form-control" id="tanggal_lahir" name="tanggal_lahir"
-                                        value="<?=$tanggal_lahir?>" required>
+                                        value="<?=$tanggal_lahir?>" <?php  if( $id_status_verifikasi == '2' OR $id_status_verifikasi == '4'){
+                                        echo 'disabled';
+                                    }else{
+                                        echo 'required';
+                                    }  ?>>
                                 </div>
 
                                 <div class="form-group">
                                     <label for="foto_kk">Foto Kartu Keluarga</label>
-                                    <input type="file" class="form-control" id="foto_kk" name="foto_kk" required>
+                                    <input type="file" class="form-control" id="foto_kk" name="foto_kk" <?php  if( $id_status_verifikasi == '2' OR $id_status_verifikasi == '4'){
+                                        echo 'disabled';
+                                    }else{
+                                        echo 'required';
+                                    }  ?>>
                                     <input type="text" id="foto_kk_old" name="foto_kk_old" value="<?=$foto_kk?>" hidden>
                                     <small id="foto_kk" class="form-text text-muted">Format
                                         PNG/JPG/JPEG (Max
