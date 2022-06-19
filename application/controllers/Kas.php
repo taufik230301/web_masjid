a00972c0d1fe8d18ef6218ff90203518ef088ced
Taufiiqul Hakim <51141717+taufik230301@users.noreply.github.com>
2022-06-19 07:25:26 +0700

    add modal add kas

--- application/controllers/Kas.php
+++ application/controllers/Kas.php
@@ -3,10 +3,32 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Kas extends CI_Controller {
 
+	public
+
 	public function view_admin()
 	{
 		$this->load->view('admin/kas');
-    }
+	}
+	
+	public function tambah_data_admin()
+	{
+
+		$jenis_kas = $this->input->post('jenis_kas');
+		$nominal = $this->input->post('nominal');
+		$keterangan_kas = $this->input->post('keterangan_kas');
+
+		$hasil = $this->m_kas->insert_kas($jenis_kas, $nominal, $keterangan_kas);
+
+        if($hasil==false){
+                $this->session->set_flashdata('eror_input','eror_input');
+                redirect('Iuran/view_admin');
+        }else{
+          		$this->session->set_flashdata('input','input');
+         		 redirect('Iuran/view_admin');
+        }
+
+
+	}
     
     public function view_bendahara()
 	{
--- application/views/admin/kas.php
+++ application/views/admin/kas.php
@@ -10,8 +10,8 @@
 
         <!-- Preloader -->
         <div class="preloader flex-column justify-content-center align-items-center">
-            <img class="animation__shake" src="<?=base_Url();?>assets/admin_lte/dist/img/Loading.png"
-                alt="AdminLTELogo" height="60" width="60">
+            <img class="animation__shake" src="<?=base_Url();?>assets/admin_lte/dist/img/Loading.png" alt="AdminLTELogo"
+                height="60" width="60">
         </div>
 
         <?php $this->load->view('admin/components/navbar');?>
@@ -35,6 +35,10 @@
                                 <li class="breadcrumb-item active">Kas</li>
                             </ol>
                         </div><!-- /.col -->
+                        <button type="button" class="btn btn-primary ml-2 mt-3" data-toggle="modal"
+                            data-target="#exampleModal">
+                            Tambah Data
+                        </button>
                     </div><!-- /.row -->
                 </div><!-- /.container-fluid -->
             </div>
@@ -45,7 +49,7 @@
                 <div class="container-fluid">
                     <div class="row">
                         <div class="col-12">
-                           
+
                             <!-- /.card -->
 
                             <div class="card">
@@ -74,7 +78,7 @@
                                                 <td>X</td>
                                             </tr>
                                         </tbody>
-                                       
+
                                     </table>
                                 </div>
                                 <!-- /.card-body -->
@@ -86,11 +90,45 @@
                     <!-- /.row -->
                 </div>
                 <!-- /.container-fluid -->
+                <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel"
+                    aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalLabel">Tambah Data Kematian</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                <form action="<?=base_url();?>Kas/tambah_data_admin" method="POST">
+                                    <div class="form-group">
+                                        <label for="jenis_kas">Jenis Kas</label>
+                                        <select class="form-control" id="jenis_kas" name="jenis_kas">
+                                            <option value="Debit">Debit</option>
+                                            <option value="Kredit">Kredit</option>
+                                        </select>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="nominal">Nominal</label>
+                                        <input type="text" class="form-control" id="nominal" name="nominal" required>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="keterangan_kas">Keterangan Kas</label>
+                                        <textarea class="form-control" id="keterangan_kas" rows="3"
+                                            name="keterangan_kas" required></textarea>
+                                    </div>
+                                    <button type="submit" class="btn btn-primary">Submit</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </section>
             <!-- /.content -->
             <!-- /.content -->
         </div>
-      
+
 
         <!-- Control Sidebar -->
         <aside class="control-sidebar control-sidebar-dark">
