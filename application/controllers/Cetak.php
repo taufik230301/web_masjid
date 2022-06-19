9684084f4ae6f5af9d283e19e8e6cb2978fba7c3
Taufiiqul Hakim <51141717+taufik230301@users.noreply.github.com>
2022-06-19 20:06:41 +0700

    add file cetak

--- application/controllers/Cetak.php
+++ application/controllers/Cetak.php
@@ -5,7 +5,18 @@ class Cetak extends CI_Controller {
 
 	public function kartu_anggota()
 	{
-		echo "cetak kartu anggota";
+		$data = array(
+			"dataku" => array(
+				"nama" => "Petani Kode",
+				"url" => "http://petanikode.com"
+			)
+		);
+	
+		$this->load->library('pdf');
+	
+		$this->pdf->setPaper('A4', 'potrait');
+		$this->pdf->filename = "laporan-petanikode.pdf";
+		$this->pdf->load_view('laporan_pdf', $data);
     }
     
 	
